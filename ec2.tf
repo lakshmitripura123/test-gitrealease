a488bf93a3ac71f79cabf8eb7aefb516455d99c7
lakshmitripura123 <lakshmitripura542@gmail.com>
2023-02-12 15:39:36 +0530

    fix

--- ec2.tf
+++ ec2.tf
@@ -22,3 +22,11 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
+resource "aws_instance" "app_server1" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
